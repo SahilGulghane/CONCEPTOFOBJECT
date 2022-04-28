d69618b5bde49205ede6f2eacb413019772781a2
SahilGulghane <101868182+SahilGulghane@users.noreply.github.com>
2022-04-28 11:24:43 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    Student S1 = new Student();
+        S1.roll_no = 33;
+        System.out.println(S1.roll_no);
+    }
+}
+class Student {
+
+    int roll_no ;
+
+}
